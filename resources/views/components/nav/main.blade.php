0c5bf9f55f6fd6b03feb46070e2b6042e5445011
Tom Hine <tomhine792@gmail.com>
2022-05-02 14:11:27 +0100

    skipped reading list if no user or no saved posts

--- resources/views/components/nav/main.blade.php
+++ resources/views/components/nav/main.blade.php
@@ -1,11 +1,18 @@
 @php
-$links = ['home', 'listings', 'podcasts', 'videos', 'tags', 'FAQ', 'forem-shop', 'sponsors', 'about', 'contact'];
+$links = ['home', 'reading-list', 'listings', 'podcasts', 'videos', 'tags', 'FAQ', 'forem-shop', 'sponsors', 'about',
+'contact'];
 @endphp
 
 <nav>
     <ul>
         @foreach ($links as $link)
-        <x-link.button-hover href="/{{ $link === 'home' ? '' : '#' }}" class="px-2 capitalize">
+        @php
+        if ($link === 'reading-list') {
+        if (!Auth::user()) continue;
+        if (!Auth::user()->bookmarks->count()) continue;
+        }
+        @endphp
+        <x-link.button-hover href="/{{ $link === 'home' ? '' : $link }}" class="px-2 capitalize">
             <span class="max-h-6 max-w-6">
                 <x-nav.icon :icon="$link" />
             </span>
