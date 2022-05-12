a2912dde73f3766c977f6a619d1037aea5416a97
Tom Hine <tomhine792@gmail.com>
2022-05-12 10:30:32 +0100

    added content to the main page right aside

---
+++ resources/views/components/side-card.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->merge(['class' => 'bg-zinc-50 border rounded-lg p-4 rounded-md flex flex-col gap-4']) }}>
+    {{ $slot }}
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,7 @@
+@php
+$speakers = ['Angie Jones', 'Kelsey Hightower', 'Tracy Chou'];
+@endphp
+
 <x-app-layout>
     <div class="flex justify-center gap-4 w-full max-w-7xl">
         <!-- left sidebar -->
@@ -16,6 +20,44 @@
             @endforeach
         </div>
         <!-- right sidebar -->
-        <aside class="hidden lg:flex flex-col w-1/4 max-w-xs bg-zinc-50 border rounded-lg"></aside>
+        <aside class="hidden lg:flex flex-col gap-4 w-1/4 max-w-xs">
+            <x-side-card>
+                <img src="{{ asset('images/aside/codeland.png') }}" alt="" aria-hidden>
+                <div class="border-b-2 border-gray-700 py-2">
+                    <em class="font-bold text-gray-700">Codeland Keynote Speakers,
+                        Announced!</em>
+                </div>
+                <div>
+                    <ul>
+                        @foreach ($speakers as $speaker)
+                        <li class="text-brand-purple font-bold">&rarr; {{ $speaker }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </x-side-card>
+            <x-side-card class="text-brand-purple font-black gap-6">
+                <img src="{{ asset('images/aside/forem-app.png') }}" alt="" class="rounded-md" aria-hidden>
+                <div>
+                    <strong class="text-2xl">Forem Android App is Here</strong>
+                </div>
+                <div>
+                    <strong class="text-lg">It's the best place to regularly use DEV on mobile</strong>
+                </div>
+                <div class="py-4">
+                    <p class="text-gray-800 font-normal">(We already have an iOS app out)</p>
+                </div>
+            </x-side-card>
+            <x-side-card class="text-gray-700">
+                <img src="{{ asset('images/aside/forem-jobs.png') }}" alt="" class="rounded-md" aria-hidden>
+                <div>
+                    <strong class="text-2xl font-bold">Open Engineering Roles @ Forem!</strong>
+                </div>
+                <div class="py-4">
+                    <p class="font-bold">Forem is the open source software that runs DEV and a number of other
+                        passionate communities. Interested in joining our global, remote-first team and helping us build
+                        a better internet?</p>
+                </div>
+            </x-side-card>
+        </aside>
     </div>
 </x-app-layout>
