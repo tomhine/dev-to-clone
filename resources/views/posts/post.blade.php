8f418f4e09d09d23cd49e1b7310e3563e8f3be5f
Tom Hine <tomhine792@gmail.com>
2022-05-12 14:53:49 +0100

    added user info box to aside on post page

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout :title="$post->title">
-    <div class="flex gap-2 w-full max-w-7xl px-4">
+    <div class="flex gap-4 w-full max-w-7xl px-4">
         <div class="w-16">
             <aside class="flex flex-col items-center gap-[17px] w-16 fixed top-[118px]">
                 <livewire:like-button :post='$post' />
@@ -29,5 +29,34 @@
                 </div>
             </section>
         </article>
+        <aside class="relative hidden lg:block w-full lg:max-w-[280px] xl:max-w-[345px]">
+            <x-side-card
+                class="border-t-[32px] border-t-gray-900 max-w-[260px] xl:max-w-[345px] fixed top-[73px] gap-6">
+                <div class="flex gap-4 -mt-8">
+                    <img src="{{ asset('images/avatar.png') }}" alt="user avatar" class="h-12 w-12 rounded-full">
+                    <h2 class="self-end font-bold text-xl text-gray-700">{{ $post->author->name }}</h2>
+                </div>
+                <x-button.hover class="justify-center">Follow</x-button.hover>
+                <div>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt autem deleniti excepturi maxime
+                        sint fugit blanditiis possimus laudantium, quisquam perferendis facilis ipsa. Maxime distinctio
+                        libero quod autem ratione, minus delectus?</p>
+                </div>
+                <div class="space-y-4">
+                    <div>
+                        <h3 class="uppercase text-gray-700 font-bold text-xs">Location</h3>
+                        <address class="not-italic">Manchester, UK</address>
+                    </div>
+                    <div>
+                        <h3 class="uppercase text-gray-700 font-bold text-xs">Work</h3>
+                        <p class="not-italic">Frontend Dev</p>
+                    </div>
+                    <div>
+                        <h3 class="uppercase text-gray-700 font-bold text-xs">Joined</h3>
+                        <p class="not-italic">{{ $post->author->created_at->format('d F Y') }}</p>
+                    </div>
+                </div>
+            </x-side-card>
+        </aside>
     </div>
 </x-app-layout>
