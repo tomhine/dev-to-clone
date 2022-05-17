af55b67e9290c3c18d898b0bd3957665b80c5c85
Tom Hine <tomhine792@gmail.com>
2022-05-18 00:18:26 +0100

    moved reading list controller stuff into bookmark controller

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bookmark;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BookmarkController extends Controller
 {
-    //
+    /**
+     * Return the reading list view with the current users bookmarked posts
+     *
+     * @return void
+     */
+    public function index()
+    {
+        return view('reading-list', [
+            'bookmarks' => Bookmark::with(['post', 'user'])->where('user_id', Auth::user()->id)->get()
+        ]);
+    }
 }
